1d1f78eead983978a8fde39accfcd42c7de5c60a
dariofa <dariojavier99@hotmail.com>
2016-06-06 01:53:53 -0500

    se creo la carpeta requires, asi mismo se creo el menu.php modelo de bootstrap

---
+++ requires/menu.php
@@ -0,0 +1,48 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <a class="navbar-brand" href="#">Ova Electiva II</a>
+        <li class="active"><a href="/?op=0">Tipos de recursos <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Recursos</a></li>
+         <!--    
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Buscar por año</a></li>
+            <li><a href="#">Contactenos</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>-->
+      </ul>
+ 
+      
+  
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Accede<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#" data-toggle="modal" data-target="#myModal">Entrar</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Registrarse</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Link Desplegable <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Opción 1</a></li>
+            <li><a href="#">Opción 2</a></li>
+            <li><a href="#">Opción 3</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Opción 4</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
