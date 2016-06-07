99f6ebf64ac9402401e4dfd507b4ffa0819272a3
caparra92 <caparra1992@gmail.com>
2016-06-07 12:48:57 -0500

    Modificación de campos input en formulario agregar usuario

--- admin/frmaddusuario.php
+++ admin/frmaddusuario.php
@@ -5,8 +5,8 @@ include (conex.php);
 	<div class="titulo-add-recurso">Agregar Usuario</div>
 	<form role="form" method="post" action="addusuario.php">
   <div class="form-group">
-    <label for="tipo_id">Tipo identificación</label>
-    <select required class="form-control" id="tipo_id" name="tipo_id">
+    <label for="tipo_identificacion">Tipo identificación</label>
+    <select required class="form-control" id="tipo_id" name="tipo_identificacion">
         <option value="1">Cedula de ciudadanía</option>
         <option value="2">Targeta de Identidad</option>
         <option value="3">Cedula Extrangería</option>
@@ -14,17 +14,16 @@ include (conex.php);
     </select>
   </div>
   <div class="form-group">
-      <label for="numero_id">Numero de identificación</label>
-      <input type="text" class="form-control" name="numero_id">
+      <label for="numero_identificacion">Numero de identificación</label>
+      <input type="text" class="form-control" name="numero_identificacion">
   </div> 
   <div class="form-group">
-      <label for="nom_usuario">Nombre usuario</label>
-      <input type="text" class="form-control" name="nom_usuario">
+      <label for="nombre_usuario">Nombre usuario</label>
+      <input type="text" class="form-control" name="nombre_usuario">
   </div>            
-  <button type="submit" class="btn btn-success" name="btn-add_user">Agregar</button>
   <div class="form-group">
-    <label for="rol">Rol</label>
-    <select required class="form-control" id="tipo_id" name="rol">
+    <label for="id_rol">Rol</label>
+    <select required class="form-control" id="id_rol" name="id_rol">
         <option value="1">Docente</option>
         <option value="2">Estudiante</option>
         <option value="3">Administrador</option>
