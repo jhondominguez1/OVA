b08882fae90919b8bf84dea15df34812279f4e5c
MichaelStevenDelgado <miesdeme@gmail.com>
2016-06-08 15:18:39 -0500

    SE REALIZAN CORRECCIONES EN FORMULARIO DE CAMILO PARRA, CON EL FIN DE QUE EL ADMINISTRADOR PUEDA AGREGAR USUARIOS, QUEDA INGRESANDO DATOS.... NADA DE ESTILOS

--- admin/addusuario.php
+++ admin/addusuario.php
@@ -1,14 +1,13 @@
 <?php
 
-include ("conex.php");
+include ("conexion.php");
 if (isset($_POST['tipo_identificacion']) && !empty($_POST['tipo_identificacion']) && isset($_POST['numero_identificacion']) &&!empty($_POST['numero_identificacion'])&& isset($_POST['nombre_usuario']) &&!empty($_POST['nombre_usuario'])&& isset($_POST['id_rol']) &&!empty($_POST['id_rol'])){
 	mysqli_query($conexion, "INSERT INTO usuarios(tipo_identificacion,numero_identificacion,nombre_usuario,id_rol) VALUES('$_POST[tipo_identificacion]','$_POST[numero_identificacion]','$_POST[nombre_usuario]','$_POST[id_rol]')") or die ("Problemas en la consulta");
 	//echo "Los datos han sido insertados con exito";
-	header("location:#");
+	echo"<script type=\"text/javascript\">alert('Los datos se insertaron correctamente'); window.location='frmaddusuario.php';</script>";
+        
 }
 else{
-	echo"<script type=\"text/javascript\">alert('Debe digitar los datos para ser insertados'); window.location='index.php';</script>";
+	echo"<script type=\"text/javascript\">alert('Debe digitar los datos para ser insertados'); window.location='frmaddusuario.php';</script>";
 }
-echo "LINEA DE ENSAYO"
-
 ?>
--- admin/frmaddusuario.php
+++ admin/frmaddusuario.php
@@ -1,36 +1,45 @@
+<html>
+    <title> Formulario Agregar Usuario</title>
+    <head> 
+       <meta charset="UTF-8">
 <?php
-include (conex.php);
+include ("conexion.php");
 ?>
+    </head>
+    <body>
+        <table align="center"><tr><td colspan="2" align="center" >
 <div class="form-add-trec">
-	<div class="titulo-add-recurso">Agregar Usuario</div>
-	<form role="form" method="post" action="addusuario.php">
+    <div class="titulo-add-recurso">Agregar Usuario</div></td>
+	<tr><td><form role="form" method="post" action="addusuario.php">
   <div class="form-group">
-    <label for="tipo_identificacion">Tipo identificación</label>
-    <select required class="form-control" id="tipo_id" name="tipo_identificacion">
-        <option value="1">Cedula de ciudadanía</option>
-        <option value="2">Targeta de Identidad</option>
-        <option value="3">Cedula Extrangería</option>
-        <option value="4">Pasaporte</option>
-    </select>
-  </div>
-  <div class="form-group">
-      <label for="numero_identificacion">Numero de identificación</label>
-      <input type="text" class="form-control" name="numero_identificacion">
-  </div> 
-  <div class="form-group">
-      <label for="nombre_usuario">Nombre usuario</label>
-      <input type="text" class="form-control" name="nombre_usuario">
-  </div>            
-  <div class="form-group">
-    <label for="id_rol">Rol</label>
-    <select required class="form-control" id="id_rol" name="id_rol">
-        <option value="1">Docente</option>
-        <option value="2">Estudiante</option>
-        <option value="3">Administrador</option>
-    </select>
-  </div>
-  <button type="submit" class="btn btn-success" name="btn-add_user">Agregar</button>
+      <div><label for="tipo_identificacion">Tipo identificación</label></td><td>
+    <select required class="form-control" id="tipo_identificacion" name="tipo_identificacion">
+        <option value="cedula">Cedula de ciudadanía</option>
+        <option value="tarjeta">Targeta de Identidad</option>
+        <option value="cedula ex">Cedula Extrangería</option>
+        <option value="pasaporte">Pasaporte</option>
+    </select></div>
+  </div></td>
+  <tr><td><div class="form-group">
+      <label for="numero_identificacion">Numero de identificación</label></td>
+      <td><input type="text" class="form-control" id="numero_identificacion" name="numero_identificacion">
+  </div> </td></tr>
+ <tr><td> <div class="form-group">
+      <label for="nombre_usuario">Nombre usuario</label></td>
+      <td><input type="text" class="form-control" id="nombre_usuario" name="nombre_usuario">
+  </div>  </td></tr>          
+  <tr><td><div class="form-group">
+    <label for="id_rol">Rol</label></td>
+    <td><select required class="form-control" id="id_rol" name="id_rol">
+        <option value="2">Docente</option>
+        <option value="3">Estudiante</option>
+        <option value="1">Administrador</option>
+        </select>
+  </div></td></tr>
+  <tr><td colspan="2"><button type="submit" class="btn btn-success" name="btn-add_user">Agregar</button></td></tr>
 </form>
 </div>
+</table>
+</body>
 
-
+</html>
