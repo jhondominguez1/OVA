d8e1c9e06b1c794de47b9970e3d619305590c508
JessGue <Jesica.guerrero1@gmail.com>
2016-06-10 16:11:18 -0500

    Se creó botón enviar y se realizo insert en preguntas.php

--- docente/preguntas.php
+++ docente/preguntas.php
@@ -7,8 +7,36 @@
   </div>
             <div class="form-group">
                 <br>
-                <input type="submit " value="enviar"/>
-            </div>
+                <button type="submit" class="btn btn-success" name="btn-add-pregunta"/>
+             </div>
 
-
+<?php
+//agrendando un nueva lección----
+	if (isset($_POST['btn-add-pregunta'])) {
+		$nombre_recurso = $_POST['nombre_pregunta'];
+		            
+	//valido que el nombre de la leccion no exista.. para ello debemos hacer la consulta a la base de datos-..
+		$sql = "SELECT * FROM preguntas  WHERE nombre_pregunta = $nombre_pregunta";
+		$query = mysqli_query($conexion,$sql);
+		$numrwos=mysqli_num_rows($query);
+		echo $sql;
+		if ($numrwos>0) {
+			echo "<script>
+				alert('El nombre ya existe...');
+				window.location='./?op=1';
+			</script>"	;
+		}else{
+			//si no existe hago el registro----
+                        /*NOTA: Esta tabla requiere el id de curso por tanto se insertara 1 como id_curso, deberá ,modificarse cuando ya
+                        esten los cursos*/
+			$sql ="INSERT INTO preguntas VALUES(NULL,'$nombre_pregunta')";
+			$query=mysqli_query($conexion,$sql);
+                        //echo $sql;
+			echo "<script>
+				alert('Datos Agregados con exito..');
+				window.location='./?op=1';
+			</script>"	;	
+			}
+	}
+?>
 
