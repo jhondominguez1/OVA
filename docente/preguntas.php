028bb605429d90e34ea62b12c25ddfe33a1ef377
marcelamora <moritamarcela2@gmail.com>
2016-06-10 10:14:19 -0500

    Cree el formulario de preguntas

---
+++ docente/preguntas.php
@@ -0,0 +1,14 @@
+<div class="form-add-trec">
+	<div class="titulo-add-recurso">Agregrar Pregunta</div>
+	<form role="form" method="post" action="">
+  <div class="form-group">
+    <label for="nom-pregunta">Nombre de la pregunta</label>
+    <input type="text" required class="form-control" id="" name="nombre_pregunta">
+  </div>
+            <div class="form-group">
+                <br>
+                <input type="submit " value="enviar"/>
+            </div>
+
+
+
