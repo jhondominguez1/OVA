1c01aea67cb7ca22d8c1ce4ab63a235cf73e3c1f
JessGue <Jesica.guerrero1@gmail.com>
2016-06-10 15:51:54 -0500

    Insert para evaluacion.php

--- docente/evaluacion.php
+++ docente/evaluacion.php
@@ -29,5 +29,34 @@
   <button type="submit" class="btn btn-success" name="btn-add-trec">Crear</button>
 </form>
 </div>
-
+<?php
+//agrendando un nueva lección----
+	if (isset($_POST['btn-add-recurso'])) {
+		$nombre_evaluacion = $_POST['nombre_evaluacion'];
+		$id_leccion = $_POST['id_leccion'];
+                    
+	//valido que el nombre de la leccion no exista.. para ello debemos hacer la consulta a la base de datos-..
+		$sql = "SELECT * FROM evaluacion  WHERE id_leccion = $id_leccion";
+		$query = mysqli_query($conexion,$sql);
+		$numrwos=mysqli_num_rows($query);
+		echo $sql;
+		if ($numrwos>0) {
+			echo "<script>
+				alert('El nombre ya existe...');
+				window.location='./?op=1';
+			</script>"	;
+		}else{
+			//si no existe hago el registro----
+                        /*NOTA: Esta tabla requiere el id de curso por tanto se insertara 1 como id_curso, deberá ,modificarse cuando ya
+                        esten los cursos*/
+			$sql ="INSERT INTO evaluacion VALUES(NULL,'$nombre_evaluacion','$id_leccion')";
+			$query=mysqli_query($conexion,$sql);
+                        //echo $sql;
+			echo "<script>
+				alert('Datos Agregados con exito..');
+				window.location='./?op=1';
+			</script>"	;	
+			}
+	}
+?>
 
