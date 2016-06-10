732ab754db31afd758b224688f6827cf657b9589
Luisf28u <luisf28u@gmail.com>
2016-06-10 18:14:16 -0500

    se aplico interfaz para registrarse en administrador se enlazo login a registro y viceversa se corrigio codigo faltante en login

---
+++ admin/conexion.php
@@ -0,0 +1,10 @@
+<?php 
+		$host ="localhost";
+		$user ="root";
+		$pass ="";
+		$bd ="ova_decimodos";
+		$conexion = mysqli_connect($host,$user,$pass,$bd);
+
+
+ ?>
+
--- admin/registro.php
+++ admin/registro.php
@@ -1,7 +1,6 @@
-<?php require_once("../conex/conexion.php"); ?>
+<?php 
+require_once("conexion.php"); 
 
- 
- <?php
  
 if(isset($_POST["register"])){
  
@@ -36,42 +35,83 @@ $result=mysql_query($sql);
  $message = "Todos los campos no deben de estar vacios!";
 }
 }
+
+if (!empty($message)) {
+echo "<p class=\error\>" . "Mensaje: ". $message . "</p>";
+} 
 ?>
+ <!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Registro</title>
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/datepicker3.css" rel="stylesheet">
+<link href="css/styles.css" rel="stylesheet">
+
+<!--[if lt IE 9]>
+<script src="js/html5shiv.js"></script>
+<script src="js/respond.min.js"></script>
+<![endif]-->
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation"><!-- texto del banner-->
+		
+			<div class="navbar-header">
+				
+				<a class="navbar-brand" href="registro.php"><span>Proyect</span>OVA</a>
+			
+			</div>		
+	</nav>
+	<br><br><br>
+
+<div class="row">
+	<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
+		<div class="login-panel panel panel-default">
+			<div class="panel-heading">
+			Registrarse
+			</div>
+				<div class="panel-body">
+					<form role="form">
+						<fieldset>
+							<div class="form-group">
+							
+								<label for="user_login">Nombre Completo<br />
+								<input type="text" name="full_name" id="full_name" class="input" size="32" value="" /></label>
+								</p>
  
-<?php if (!empty($message)) {echo "<p class=\"error\">" . "Mensaje: ". $message . "</p>";} ?>
+								<p>
+								<label for="user_pass">E-mail<br />
+								<input type="email" name="email" id="email" class="input" value="" size="32" /></label>
+								</p>
  
-<div class="container mregister">
- <div id="login">
- <h1>Registrar</h1>
-<form name="registerform" id="registerform" action="register.php" method="post">
- <p>
- <label for="user_login">Nombre Completo<br />
- <input type="text" name="full_name" id="full_name" class="input" size="32" value="" /></label>
- </p>
+								<p>
+								<label for="user_pass">Nombre De Usuario<br />
+								<input type="text" name="username" id="username" class="input" value="" size="32" /></label>
+								</p>
  
- <p>
- <label for="user_pass">E-mail<br />
- <input type="email" name="email" id="email" class="input" value="" size="32" /></label>
- </p>
+								<p>
+								<label for="user_pass">Contraseña<br />
+								<input type="password" name="password" id="password" class="input" value="" size="32" /></label>
+								</p>
  
- <p>
- <label for="user_pass">Nombre De Usuario<br />
- <input type="text" name="username" id="username" class="input" value="" size="20" /></label>
- </p>
+								<p class="submit">
+								<input type="submit" name="register" id="register" class="btn btn-primary" value="Registrar" />
+								</p>
  
- <p>
- <label for="user_pass">Contraseña<br />
- <input type="password" name="password" id="password" class="input" value="" size="32" /></label>
- </p>
+								<p class="regtext">¿Ya tienes una cuenta? <a href="login.html" >Entra Aquí.</a></p>
+							</div>
+						</fieldset>
+					</form>
  
-<p class="submit">
- <input type="submit" name="register" id="register" class="button" value="Registrar" />
- </p>
- 
- <p class="regtext">Ya tienes una cuenta? <a href="login.php" >Entra Aquí!</a>!</p>
-</form>
- 
- </div>
- </div>
- 
- <?php include("includes/footer.php"); ?>
+				</div>
+			
+		</div>
+	</div>
+</div>
+</body>
