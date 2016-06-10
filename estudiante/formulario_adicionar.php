2014cb78a60689dd9cf5104dc44edf58736f613b
marcesalazar <marcesalazar88@gmail.com>
2016-06-10 09:35:55 -0500

    estructura para la primera visualizacion del rol de estudiante

--- estudiante/formulario_adicionar.php
+++ estudiante/formulario_adicionar.php
@@ -1,6 +1,51 @@
-<?php
 
 
+    
+<html lang="es">
+<?php session_start();
+require('./conex/conexion.php');
+?>
+<head >
+  <title>Login Estudiente</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
+<meta charset="UTF-8">
 
+
+<link rel="stylesheet" href="Resources/css/bootstrap.min.css">
+
+<script src="Resources/js/jquery-1.11.2.js"></script>
+<script src="Resources/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+     <div class="panel-heading">
+      <h1>BLOG ESCOLAR</h1>
+    </div>
+
+     <div >
+      <?php 
+      require('./requires/menu.php');
+       ?>
+       </div>
+    
+     <div class="container">
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Recursos</div>
+                    <div class="panel-body">                                          
+                        <form role="form" >
+                                                
+                            <button type="button" class="btn btn-primary" ><span class="glyphicon glyphicon-lock"></span> Entrar</button>   
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+</body>
+</html>
