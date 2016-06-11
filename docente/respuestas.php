cb2cadaaf485ec80678252f009d80f862a6a64cd
javiertoro77 <toro.jav@hotmail.com>
2016-06-10 23:41:35 -0500

    insert del formulario de respuestas
    insert del formulario de respuestas

--- docente/respuestas.php
+++ docente/respuestas.php
@@ -13,8 +13,34 @@
   </div>
             <div class="form-group">
  <br/>
-   <center><button type="submit" class="btn btn-success" name="btn-add-trec">Crear Respuesta</button> </center>
+   <center><button type="submit" class="btn btn-success" name="btn-add-resp">Crear Respuesta</button> </center>
             </div>
+            
+ <?php
+//agrendando respuestas----
+	if (isset($_POST['btn-add-resp'])) {
+		$nombre_resp = $_POST['nombre_respuesta'];
+		$sql = "SELECT * FROM preguntas  WHERE nombre_pregunta = $nombre_resp";
+		$query = mysqli_query($conexion,$sql);
+		$numrwos=mysqli_num_rows($query);
+		echo $sql;
+		if ($numrwos>0) {
+			echo "<script>
+				alert('El nombre ya existe...');
+				window.location='./?op=1';
+			</script>"	;
+		}else{
+		
+			$sql ="INSERT INTO respuestas VALUES(NULL,'$nombre_resp')";
+			$query=mysqli_query($conexion,$sql);
+                        //echo $sql;
+			echo "<script>
+				alert('Respuesta agregada con exito..');
+				window.location='./?op=1';
+			</script>"	;	
+			}
+	}
+?>
    
     
 
