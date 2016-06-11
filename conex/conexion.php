e80071926ecd88c6578f2ec1f6c16f32667a8d6d
sebastianjimenez10 <juansjimenez10@gmail.com>
2016-06-11 10:20:55 -0500

    agrego mensaje de alerta, en caso de que la conexión con la bd no se realize.

--- conex/conexion.php
+++ conex/conexion.php
@@ -3,7 +3,7 @@
 		$user ="root";
 		$pass ="";
 		$bd ="ova_decimodos";
-		$conexion = mysqli_connect($host,$user,$pass,$bd);
+		$conexion = mysqli_connect($host,$user,$pass,$bd) or die ("la conexión no se realizo correctamente");
 
 
  ?>
