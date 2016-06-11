cbe112294acd02264b9413f725dbb67aacb798fd
Cristian Chamorro <cristian31093@gmail.com>
2016-06-11 09:58:21 -0500

    Se agrego ide a los input para el nombre de curso y nombre de docente en el archivo registrar_curso.php

--- admin/registrar_curso.php
+++ admin/registrar_curso.php
@@ -8,8 +8,8 @@
 
 <form action="registrar_curso.php" method="POST">
 	
-digite nombre del curso &nbsp;&nbsp;<input type="text" name="nom_curso"><br><br>
-	digite nombre del docente &nbsp;&nbsp;<input type="text" name="nom_docente"><br><br>
+digite nombre del curso &nbsp;&nbsp;<input type="text" id="nom_docente" name="nom_curso"><br><br>
+digite nombre del docente &nbsp;&nbsp;<input type="text" id="nom_docente" name="nom_docente"><br><br>
 	<input type="submit" value="Registrar curso">
 	</form>
 </body>
