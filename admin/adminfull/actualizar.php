28aba2fa82a4df439791c406d0019f613b46a77c
Cristian Danilo Jojoa <cristiandjr2@gmail.com>
2016-06-11 22:32:37 -0500

    Delete actualizar.php
    archivo no necesario

--- admin/adminfull/actualizar.php
+++
@@ -1,9 +0,0 @@
-<?php 
-//incluir archivo de conexion
-include("conexion.php");
-$id_curso=$_POST['id_curso'];
-//establecer conexion con el servidor 
-mysqli_query($conexion, "UPDATE cursos SET nombre_curso='$_POST[nombre_curso]', descripcion_curso='$_POST[descripcion_curso]', id_area_conocimiento='$_POST[id_area_conocimiento]' WHERE id_curso='$id_curso'") or die ("problemas en la conexion");
-echo "Datos actualizados de forma exitosa";
-echo "<meta http-equiv='refresh' content='2;url=gestionarcurso.php'> ";
-?>
