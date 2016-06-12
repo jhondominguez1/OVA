5652d925ecb5b158ab5cb6351b9cecde699de2b1
Luisf28u <luisf28u@gmail.com>
2016-06-11 23:36:39 -0500

    actualizando codigo faltante

--- admin/frmaddusuario.php
+++ admin/frmaddusuario.php
@@ -17,7 +17,7 @@
 
 <body>
 
-<br><br><br>
+
 <div class="row">
 	<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 		<div class="login-panel panel panel-default">
