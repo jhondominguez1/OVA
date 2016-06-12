c9e08bf9cecaf60ab4afb48a4fe51789736fa98b
Luisf28u <luisf28u@gmail.com>
2016-06-11 23:37:30 -0500

    actualizando codigo faltante

--- admin/registro.php
+++ admin/registro.php
@@ -1,46 +1,4 @@
-<?php 
-require("../conex/conexion.php");
-
- 
-if(isset($_POST["register"])){
- 
-if(!empty($_POST['full_name']) && !empty($_POST['email']) && !empty($_POST['username']) && !empty($_POST['password'])) {
- $full_name=$_POST['full_name'];
- $email=$_POST['email'];
- $username=$_POST['username'];
- $password=$_POST['password'];
- 
- $query=mysql_query("SELECT * FROM usertbl WHERE username='".$username."'");
- $numrows=mysql_num_rows($query);
- 
- if($numrows==0)
- {
- $sql="INSERT INTO usertbl
- (full_name, email, username,password)
- VALUES('$full_name','$email', '$username', '$password')";
- 
-$result=mysql_query($sql);
- 
- if($result){
- $message = "Cuenta Correctamente Creada";
- } else {
- $message = "Error al ingresar datos de la informacion!";
- }
- 
-} else {
- $message = "El nombre de usuario ya existe! Por favor, intenta con otro!";
- }
- 
-} else {
- $message = "Todos los campos no deben de estar vacios!";
-}
-}
-
-if (!empty($message)) {
-echo "<p class=\error\>" . "Mensaje: ". $message . "</p>";
-} 
-?>
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
@@ -55,21 +13,18 @@ echo "<p class=\error\>" . "Mensaje: ". $message . "</p>";
 <script src="js/html5shiv.js"></script>
 <script src="js/respond.min.js"></script>
 <![endif]-->
-
 </head>
 
 <body>
+<!-- texto del banner-->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<div class="navbar-header">
+		<a class="navbar-brand" href="registro.php"><span>Proyect</span>OVA</a>
+	</div>		
+</nav>
+<br><br><br>
 
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation"><!-- texto del banner-->
-		
-			<div class="navbar-header">
-				
-				<a class="navbar-brand" href="registro.php"><span>Proyect</span>OVA</a>
-			
-			</div>		
-	</nav>
-	<br><br><br>
-
+<!-- formulario registro-->
 <div class="row">
 	<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 		<div class="login-panel panel panel-default">
@@ -77,29 +32,36 @@ echo "<p class=\error\>" . "Mensaje: ". $message . "</p>";
 			Registrarse
 			</div>
 				<div class="panel-body">
-					<form role="form">
+				
+					<form role="form" method="post" action="addusuario.php">
 						<fieldset>
 							<div class="form-group">
 							
-								<label for="user_login">Nombre Completo<br />
-								<input type="text" name="full_name" id="full_name" class="input" size="32" value="" /></label>
+								<label for="user_login">Tipo Identificación<br />
+								<input type="text" name="tipo_identificacion" id="tipo_identificacion" class="input" size="32" value="Codigo Estudiante" readonly/></label>
 								</p>
  
 								<p>
-								<label for="user_pass">E-mail<br />
-								<input type="email" name="email" id="email" class="input" value="" size="32" /></label>
+								<label for="user_pass">Número<br />
+								<input type="text" name="numero_identificacion" id="numero_identificacion" class="input" value="" size="32" onKeypress="if (event.keyCode < 45 || event.keyCode > 57) event.returnValue = false;" /></label>
 								</p>
  
 								<p>
 								<label for="user_pass">Nombre De Usuario<br />
-								<input type="text" name="username" id="username" class="input" value="" size="32" /></label>
+								<input type="text" name="nombre_usuario" id="nombre_usuario" class="input" value="" size="32" /></label>
 								</p>
  
 								<p>
 								<label for="user_pass">Contraseña<br />
 								<input type="password" name="password" id="password" class="input" value="" size="32" /></label>
 								</p>
- 
+								<!-- oculto por defecto el rol es de estudiante-->
+								<p>
+								<input type="text" name="id_rol" id="id_rol" class="input" value="3" size="32" hidden/></label>
+								</p>
+								<input type="text" name="formulario" id="formulario" class="input" value="estudiante" size="32" hidden/></label>
+								
+								
 								<p class="submit">
 								<input type="submit" name="register" id="register" class="btn btn-primary" value="Registrar" />
 								</p>
