2f4d49150ad7fdc2d03ea12d1f50c312d90853ca
cristiandjr2 <cristiandjr2@gmail.com>
2016-06-11 20:32:54 -0500

    cambio en llamada de archivo actualizar

--- admin/areasconocimiento.php
+++ admin/areasconocimiento.php
@@ -17,7 +17,7 @@
 
 			$registros=mysqli_query($conexion, "SELECT * FROM areas_conocimiento ")or die ("Problemas en la consulta");
 			while ($reg=mysqli_fetch_array($registros)){
-				echo "<tr><td>".$reg['nombre_area_conocimiento']."</td><td>".$reg['descripcion_area_conocimiento']."</td><td><a href=./eliminararea.php?id=".$reg['id_area_conocimiento'].">Eliminar</a></td><td><a href=./actualizararea.php?id=".$reg['id_area_conocimiento'].">Actualizar</a></td></tr>";
+				echo "<tr><td>".$reg['nombre_area_conocimiento']."</td><td>".$reg['descripcion_area_conocimiento']."</td><td><a href=./eliminararea.php?id=".$reg['id_area_conocimiento'].">Eliminar</a></td><td><a href=./frmactualizararea.php?id=".$reg['id_area_conocimiento'].">Actualizar</a></td></tr>";
 			}
 	    ?>
 		<form  action="addarea.php" method="POST">
