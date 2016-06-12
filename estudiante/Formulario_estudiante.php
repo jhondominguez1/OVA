f1979a300e7a76dbf698a7510344d16f24d19ce4
cristianpp555 <Kriztian092@gmail.com>
2016-06-12 11:11:18 -0500

    Arreglo de detalles y css a perfil de estudiante

--- estudiante/Formulario_estudiante.php
+++ estudiante/Formulario_estudiante.php
@@ -6,41 +6,55 @@
 <html lang="es">
 <head >
   <title>Login Estudiente</title>
-
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
 <meta charset="UTF-8">
-
-
 <link rel="stylesheet" href="../admin/css/bootstrap.min.css">
-
+<link rel="stylesheet" type="text/css" href="fonts/style.css">
 <script src="../admin/js/jquery-1.11.2.js"></script>
 <script src="../admin//js/bootstrap.min.js"></script>
+
 <script type="text/javascript">
   alert("Bienvedido al sistema de estudiante");
 </script>
+
 </head>
-<body>
+<body >
+  
+  <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <a class="navbar-brand" href="#">Ova Electiva II</a>
+      </ul>
+ 
+      <ul class="nav navbar-nav navbar-right">
+        <ul class="nav navbar-nav">
+        <li><a href="">Ir al Curso</a></li>
+        <li><a href="">Cerrar Sesión</a></li>
+      </ul>
 
-
-     <div class="panel-heading">
-      <h1>Perfil Estudiante</h1>
     </div>
+  </div>
+</nav>
 
-    
+
+
       <?php 
-     require('../requires/menu.php'); 
     
-      $cscursos="SELECT cursos.nombre_curso, cursos.descripcion_curso FROM cursos inner join asignacion_estudiantes on cursos.id_curso=asignacion_estudiantes.id_curso inner join usuarios on asignacion_estudiantes.id_usuario=usuarios.id_usuario inner join logins on logins.id_usuarios=usuarios.id_usuario inner join roles on roles.id_rol=usuarios.id_rol ";
-       
-    
-        $cursos=mysqli_query($conexion,$cscursos) or die("problemas en la 1 consulta".$cscursos);
+    $cscursos="SELECT cursos.nombre_curso, cursos.descripcion_curso 
+    FROM cursos 
+    inner join asignacion_estudiantes on cursos.id_curso=asignacion_estudiantes.id_curso 
+    inner join usuarios on asignacion_estudiantes.id_usuario=usuarios.id_usuario 
+    inner join logins on logins.id_usuarios=usuarios.id_usuario 
+    inner join roles on roles.id_rol=usuarios.id_rol ";
+    $cursos=mysqli_query($conexion,$cscursos) or die("problemas en la 1 consulta".$cscursos);
        ?>
        
     
     
      <div class="container">
+     <div class="well well-sm"><h4 align="center">Perfil Estudiante</h4></div>
         <div class="row">
             <div class="col-md-4">
                 <div class="row>"
@@ -70,15 +84,12 @@
            
             <div class="col-md-8">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Usuario</div>
+                    <div class="panel-heading"><b>Usuario</b></div>
                     <div class="panel-body">        
 
 
                 <form method="POST" action="actualizar-perfil-estudiante.php">                  
                 <div align="center" class="form-add-trec">
-                        <div class="form-group">
-                            <div colspan=5 align="center" class="titulo-add-recurso">Actualizar Perfil</div>
-                        </div>
                 <!-- consulta -->
                 <?php
                 include ("conexion.php");
@@ -89,19 +100,19 @@
                 ?>
                 <input type="hidden" name="id_usuario" value="<?php echo $row['id_usuario'];?>"/>
                         
-    <div class="panel panel-warning">
-    <div class="panel-heading">Documento de Identificación</div>
-      <div class="panel-body"><input type="text" name="tipo_identificacion" required class="form-control" value="<?php echo $row['tipo_identificacion'];?>"/></div>
-    </div>
-    <div class="panel panel-warning">
-      <div class="panel-heading">Número de Identificación</div>
-      <div class="panel-body"><input type="text" name="numero_identificacion" required class="form-control" value="<?php echo $row['numero_identificacion'];?>"/></div>
-    </div>
-    <div class="panel panel-warning">
-      <div class="panel-heading">Nombres y Apellidos</div>
-      <div class="panel-body"><input type="text" name="nombre_usuario" required class="form-control" value="<?php echo $row['nombre_usuario'];?>"/></div>
-    </div>
-    <div><input type="submit" value="Actualizar Datos" class="btn btn-warning"></div>
+                <div class="panel panel-warning">
+                <div class="panel-heading">Documento de Identificación</div>
+                  <div class="panel-body"><input type="text" name="tipo_identificacion" required class="form-control" value="<?php echo $row['tipo_identificacion'];?>"/></div>
+                </div>
+                <div class="panel panel-warning">
+                  <div class="panel-heading">Número de Identificación</div>
+                  <div class="panel-body"><input type="text" name="numero_identificacion" required class="form-control" value="<?php echo $row['numero_identificacion'];?>"/></div>
+                </div>
+                <div class="panel panel-warning">
+                  <div class="panel-heading">Nombres y Apellidos</div>
+                  <div class="panel-body"><input type="text" name="nombre_usuario" required class="form-control" value="<?php echo $row['nombre_usuario'];?>"/></div>
+                </div>
+                <div><input type="submit" value="Actualizar Datos" class="btn btn-warning"></div>
 
                 <?php }?>
                 </div>
@@ -112,5 +123,11 @@
             </div> 
             </div>
         </div>
+
+<footer class="footer text-center" style="color:grey; background:#222222">
+  <p>Universidad de Nariño <br>© All RIGHTS RESERVED<br>2016<br></p>
+</footer>
+
+
 </body>
 </html>
