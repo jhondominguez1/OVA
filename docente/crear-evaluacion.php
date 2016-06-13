65ea85b735cde5ae0429c3ab9e381dc3143addf4
dariofa <dariojavier99@hotmail.com>
2016-06-12 22:26:34 -0500

    Se creo el algoritmo para insertar los datos a las respectivas tablas. evaluaciones_preguntas preguntas_respuestas

--- docente/crear-evaluacion.php
+++ docente/crear-evaluacion.php
@@ -19,33 +19,32 @@
   
   	<div class="titulo-add-recurso">Creando Evaluaciones</div>
 <br>
+<form role="form" method="post" action="">
   <div class="form-group">
 	<label for="id_evaluacion">Seleccione una Evaluación</label>
 		<select class="form-control"  name="id_evaluacion" id="id_evaluacion">
-			<option value="">Ninguno</option>
+			
 						<?php
 						$consulta_evaluacion="SELECT * FROM evaluacion";
-						$consulta_evaluacion2=mysqli_query($conexion,$consulta_evaluacion) or die ("PROBLEMAS DE CONSULTA");
-						while ($fila=mysqli_fetch_array($consulta_evaluacion2)) {
+						$query=mysqli_query($conexion,$consulta_evaluacion) or die ("PROBLEMAS DE CONSULTA");
+						while ($fila=mysqli_fetch_array($query)) {
 						?>
-						<option value="<?php echo $fila[0] ?>"><?php echo "$fila3[1]" ?></option> 
+						<option value="<?php echo $fila[0] ?>"><?php echo "$fila[1]" ?></option> 
 						<?php			
 						}	
 						?>
-
 		</select>
 </div> 
- 
-   <div class="form-group">
+<div class="form-group">
 	<label for="id_pregunta">Seleccione una Pregunta</label>
 	<select class="form-control"  name="id_pregunta" id="id_pregunta">
-		<option value="">Ninguno</option>
+		
 					<?php
 					$consulta_pregunta="SELECT * FROM preguntas";
 					$consulta_pregunta2=mysqli_query($conexion,$consulta_pregunta) or die ("PROBLEMAS DE CONSULTA");
 					while ($fila=mysqli_fetch_array($consulta_pregunta2)) {
 					?>
-					<option value="<?php echo $fila[0] ?>"><?php echo "$fila3[1]" ?></option> 
+					<option value="<?php echo $fila[0] ?>"><?php echo "$fila[1]" ?></option> 
 					<?php			
 					}	
 					?>
@@ -54,15 +53,12 @@
  </div>  
   
  
-	<form role="form" method="post" action="">
+	
   <div class="form-group">
     <label for="num_pregunta">Numero de Respuestas</label>
     <input type="text" required class="form-control" id="" name="num_pregunta">
-  </div>
-  
-  
- 
- <center><button type="submit" class="btn btn-success" name="btn-add-trec">Crear Preguntas</button> </center>
+</div> 
+ <center><button type="submit" class="btn btn-success" name="btn-crea-preg">Crear Preguntas</button> </center>
 </form>
 </div>
 
@@ -74,34 +70,92 @@
   <br>
   <div class="form-group">
 	<label for="id_respuesta" class="valor">Seleccione una Respuesta</label>  <label for="value_pregunta">Asigne valor</label>
+
 	<form role="form" method="post" action="">
 	<div class="row">
-	<div class="col-md-6">
-	<select class="form-control"  name="id_respuesta" id="id_respuesta">
-		<option value="">Ninguno</option>
-					<?php
-					$consulta_evaluacion="SELECT * FROM evaluacion";
+	<input type="text" value="<?php echo $_POST['id_pregunta'] ?>" name="id_pregunta">
+	<input type="text" value="<?php echo $_POST['id_evaluacion'] ?>" name="id_evaluacion">
+
+	<?php 
+	
+		if (isset($_POST['btn-crea-preg'])) {
+			
+			$num_pregunta = $_POST['num_pregunta'];
+			
+		
+		for ($i=0; $i < $num_pregunta ; $i++) { ?>
+		<div class="col-md-6">
+				<select class="form-control"  name="id_respuesta" id="id_respuesta">
+				<?php
+					$consulta_evaluacion="SELECT * FROM respuestas";
 					$consulta_evaluacion2=mysqli_query($conexion,$consulta_evaluacion) or die ("PROBLEMAS DE CONSULTA");
 					while ($fila=mysqli_fetch_array($consulta_evaluacion2)) {
 					?>
-					<option value="<?php echo $fila[0] ?>"><?php echo "$fila3[1]" ?></option>
+					<option value="<?php echo $fila[0] ?>"><?php echo "$fila[1]" ?></option>
 					<?php			
 					}					
-					?>
-	</select>
-	</div>
+				?>
+			</select>
+		</div>
+			<div class="col-md-6">
+				<input type="text" class="form-control" id="" name="value_pregunta">
+			</div>
+	<?php } } ?>	
 	
-	<div class="col-md-6">
-	<input type="text" class="form-control" id="" name="value_pregunta">
-	</div>
+	
+	
+	
  </div> 
  <br>
  <br>
- <center><button type="submit" class="btn btn-success" name="btn-add-trec">Crear Evaluacion</button> </center>
+ <center><button type="submit" class="btn btn-success" name="btn-add-evaluacion">Crear Evaluacion</button> </center>
 </form>
 </div>
 </div>		
 		
-<script type="text/javascript" src="../js/jquery.js"></script>
-		<script type="text/javascript" src="../js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="../js/java.js"></script>	
+		<?php 
+
+		if (isset($_POST['btn-add-evaluacion'])) {
+		
+                    
+	//valido que el nombre de la leccion no exista.. para ello debemos hacer la consulta a la base de datos-..
+		//$sql = "SELECT * FROM evaluacion  WHERE nombre_evaluacion = '$nombre_evaluacion'";
+		//$query = mysqli_query($conexion,$sql);
+		//$numrwos=mysqli_num_rows($query);
+		//echo $sql;
+		/*if ($numrwos>0) {
+			echo "<script>
+				alert('El nombre ya existe...');
+				window.location='./';
+			</script>"	;
+		}else{
+
+		 }*/
+			//si no existe hago el registro----
+                        /*NOTA: Esta tabla requiere el id de curso por tanto se insertara 1 como id_curso, deberá ,modificarse cuando ya
+                        esten los cursos*/
+			$sql1 ="INSERT INTO evaluaciones_preguntas VALUES($_POST[id_evaluacion],$_POST[id_pregunta])";
+			$query=mysqli_query($conexion,$sql1);
+			echo "$sql1";
+			foreach ($_POST as $key => $value) {
+				//echo $key."<br>";
+				//echo $value."<br>";
+
+			$sql ="INSERT INTO preguntas_respuestas VALUES($_POST[id_pregunta],$value,$_POST[value_pregunta])";
+			$query=mysqli_query($conexion,$sql);
+			echo "$sql <br>";
+			}
+
+                        //echo $sql;
+			echo "<script>
+				alert('Datos Agregados con exito..');
+				
+			</script>"	;	
+			
+	}
+
+
+
+
+		 ?>
+
