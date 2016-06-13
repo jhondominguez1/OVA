a715a81b8505905ad1cff27d67198e29a20fc089
juannog <Zobiak1991@gmail.com>
2016-06-13 00:05:44 -0500

    se modifico link en Formulario_estudiante.php de Electiva II y error de escritura

--- estudiante/Formulario_estudiante.php
+++ estudiante/Formulario_estudiante.php
@@ -5,7 +5,7 @@
 ?>
 <html lang="es">
 <head >
-  <title>Login Estudiente</title>
+  <title>Login Estudiante</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta charset="UTF-8">
 <link rel="stylesheet" href="../admin/css/bootstrap.min.css">
@@ -25,7 +25,7 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <a class="navbar-brand" href="#">Ova Electiva II</a>
+        <a class="navbar-brand" href="../index.php">Ova Electiva II</a>
       </ul>
  
       <ul class="nav navbar-nav navbar-right">
