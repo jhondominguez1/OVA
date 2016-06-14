4b3942989655d7bfb28654f048dd0f4eb62ff638
cristiandjr2 <cristiandjr2@gmail.com>
2016-06-13 22:36:23 -0500

    update eliminar_usuario para eliminar en cascada  con asignaciones  docentes

--- admin/eliminarusuario.php
+++ admin/eliminarusuario.php
@@ -1,11 +1,13 @@
 <?php
 require("../conex/conexion.php");
 $id=$_GET['id'];
-$registro=mysqli_query($conexion,"SELECT * FROM usuarios  WHERE id_usuario='$id'") or die ("Problema en la consulta");
-if ($reg=mysqli_fetch_array($registro)){
-	
-	
-	mysqli_query($conexion, "DELETE FROM usuarios WHERE id_usuario='$id'") or die ("Problema en la  consulta");
+$registro1=mysqli_query($conexion,"SELECT * FROM asignacion_docentes  WHERE id_usuario='$id'") or die ("Problema en la consulta 1");
+$registro=mysqli_query($conexion,"SELECT * FROM usuarios  WHERE id_usuario='$id'") or die ("Problema en la consulta2");
+if ($reg=mysqli_fetch_array($registro1)){
+	mysqli_query($conexion, "DELETE FROM asignacion_docentes WHERE id_usuario='$id'") or die ("Problema en la  consulta3");
+	}
+if ($reg=mysqli_fetch_array($registro)){	
+	mysqli_query($conexion, "DELETE FROM usuarios WHERE id_usuario='$id'") or die ("Problema en la  consulta4");
 	echo"<script type=\"text/javascript\">alert('Usuario eliminado con exito'); window.location='listarusuario.php';</script>";
 	}
 ?>
