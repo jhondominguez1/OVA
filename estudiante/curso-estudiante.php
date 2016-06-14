f6e19c0a2c63e6129ae6d4eda9d3ad779d98814e
marcesalazar <marcesalazar88@gmail.com>
2016-06-14 12:57:00 -0500

    se reizaron consultas para la visualización

--- estudiante/curso-estudiante.php
+++ estudiante/curso-estudiante.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if(!isset($_SESSION['id_usuario'])){
+         header('Location: login.php');
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -9,40 +15,21 @@
   <link rel="stylesheet" type="text/css" href="css/estilos.css">
   <link rel="stylesheet" type="text/css" href="fonts/style.css">
 
-  <script type='text/javascript' src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
- 
-    <!-- Acción sobre el botón con id=boton y actualizamos el div con id=capa -->
-    <script type="text/javascript">
-      $(document).ready(function() {
-        $("#boton1").click(function(event) {
-          $("#capa").load('parte1curso.php');
-        });
-      });
-    </script>
-    <script type="text/javascript">
-      $(document).ready(function() {
-        $("#boton2").click(function(event) {
-          $("#capa").load('parte2curso.php');
-        });
-      });
-    </script>
-    <script type="text/javascript">
-      $(document).ready(function() {
-        $("#boton3").click(function(event) {
-          $("#capa").load('parte3curso.php');
-        });
-      });
-    </script>
-    <script type="text/javascript">
-      $(document).ready(function() {
-        $("#boton4").click(function(event) {
-          $("#capa").load('parte4curso.php');
-        });
-      });
-    </script>
+
 </head>
 <body class="t1">
 
+<?php
+include("conexion.php");
+$id_usu=$_SESSION['id_usuario'];
+            echo "este es el id".$id_usu;
+            $cscursos="SELECT cursos.nombre_curso, cursos.descripcion_curso,cursos.id_curso, leccion.id_leccion FROM cursos 
+                  inner join asignacion_estudiantes on cursos.id_curso=asignacion_estudiantes.id_curso 
+                  inner join usuarios on asignacion_estudiantes.id_usuario=usuarios.id_usuario 
+                  inner join roles on roles.id_rol=usuarios.id_rol inner join leccion on leccion.id_curso=cursos.id_curso where usuarios.id_usuario=".$id_usu;
+                  echo $cscursos;
+                   $cursos=mysqli_query($conexion,$cscursos) or die("problemas en la 1 consulta".$cscursos);
+            ?>
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <!-- Collect the nav links, forms, and other content for toggling -->
@@ -52,9 +39,17 @@
           <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Seleccione Curso<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Biología</a></li>
-            <li><a href="#">Matemáticas</a></li>
-            <li><a href="#">Lenguaje</a></li>
+            <?php 
+         
+
+                
+                while ($cusu=mysqli_fetch_array($cursos)){
+                   echo "<li><a href='curso-estudiante.php?id=".$cusu[3]."'>".$cusu[0]."</a></li>";
+              
+                  }
+
+                   ?>
+            
           </ul>
         </li>
       </ul>
@@ -71,13 +66,37 @@
   </div>
 </nav>
 
+<?php
+if (isset($_GET['id'])) {
+  $c=$_GET['id'];
+    echo "este es el curso actual".$c;
+    $sql1="SELECT leccion.id_leccion,recursos.id_recurso,recursos.nombre_recurso,evaluacion.id_evaluacion, evaluacion.nombre_evaluacion from leccion
+inner join recursos on leccion.id_leccion=recursos.id_leccion
+inner join evaluacion on evaluacion.id_leccion=leccion.id_leccion where leccion.id_leccion=".$c;
 
-<table width="100%" height="400" border="0" class="t1">
-  <tr>
-    <td width="20%">
-    <div class="col-sm-2 sidenav">
-      <div class="well">
-       <button name="boton" id="boton1" type="button" class="btn btn-primary btn-lg">Comenzar Curso</button>
+//echo $sql1;
+
+$resul=mysqli_query($conexion,$sql1) or die("problemas en la 1 consulta".$sqll);
+
+?> 
+<div data-role="row">
+  <div class="col-md-4">
+    <div data-rol="row">
+         <div class="col-sm-2 sidenav">
+         <div class="well">
+            <?php 
+         
+
+                
+                while ($leccion=mysqli_fetch_array($resul)){
+                   echo "<li><button name='boton' id='".$leccion[1]."' type='button' class='btn btn-primary btn-lg'>".$leccion[2]."</a></li>";
+              
+                 }
+
+                   ?>
+
+      
+
       </div>
       <div class="well">
        <button name="boton" id="boton2" type="button" class="btn btn-primary btn-lg">Contenido</button>
@@ -88,6 +107,20 @@
        <div class="well">
        <button name="boton" id="boton4" type="button" class="btn btn-primary btn-lg">Notas</button>
       </div>
+    </div>
+  </div>
+  <div class="col-md-8">
+  </div>
+</div>
+  <?php
+   
+  # code...
+}
+?>
+<table width="100%" height="500" border="0" class="t1">
+  <tr>
+    <td width="20%">
+   
     </td>
 
     <td  width="80%" align="center">
@@ -98,7 +131,6 @@
     
     
   </tr>
-
 
 </table>
 
