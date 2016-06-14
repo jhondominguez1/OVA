ee2e3246997078f0f10f9989fab6ab587ef0572e
marcesalazar <marcesalazar88@gmail.com>
2016-06-14 12:56:06 -0500

    ahora el login funciona

--- estudiante/validarlogin.php
+++ estudiante/validarlogin.php
@@ -2,34 +2,48 @@
 <?php
 include("conexion.php");
 
-if (isset($_POST['nombre_usuario']) && !empty($_POST['nombre_usuario']) && isset($_POST['password']) &&!empty($_POST['password'])){
+$usu=$_POST['nombre_usuario'];
+$pss=$_POST['password'];
 
-$result=mysqli_query($conexion,"SELECT nombre_usuario, password, id_rol FROM usuarios WHERE nombre_usuario='$_POST[nombre_usuario]' AND password='$_POST[password]'") or die("error en la consulta");
+$i=mysqli_query($conexion,"SELECT id_usuario FROM usuarios WHERE nombre_usuario='$usu' AND password='$pss'") or die("error en la consulta");
+$r= mysqli_fetch_row($i);
+$c=$r[0];
+//echo $c;
+
+if (isset($usu) && !empty($usu) && isset($pss) &&!empty($pss)){
+
+$result=mysqli_query($conexion,"SELECT nombre_usuario, password, id_rol, id_usuario FROM usuarios WHERE nombre_usuario='$usu' AND password='$pss'") or die("error en la consulta");
 if($row = mysqli_fetch_row($result))
 	{
 		if ($row[2]==1)
 		{
 			session_start();  
 			//Almacenamos el nombre de usuario en una variable de sesión usuario
-			$_SESSION['nombre_usuario'] = $nombre_usuario;  
+			$_SESSION['nombre_usuario'] = $row=[0];  
 			//Redireccionamos a la pagina: index.php
 			header("Location: index.html");
 		}
 		elseif($row[2]==2)
 		{
 			session_start();
-			$_SESSION['nombre_usuario'] = $nombre_usuario;  
+			$_SESSION['nombre_usuario'] = $usu;  
 			header("Location: ../index.php");
 		}
 			elseif($row[2]==3)
 		{
 			session_start();
-			$_SESSION['nombre_usuario'] = $nombre_usuario;  
+
+			$_SESSION['id_usuario'] = $c;
+			 $_SESSION['nombre_usuario'] = $usu;
 			header("Location: curso-estudiante.php");
+		
 		}
 	}
 	
 }
+
+//echo $pss;
+
 echo"<script type=\"text/javascript\">alert('El nombre de usuario o la contraseña son incorrectos!'); window.location='login.php';</script>";
 
 ?>
