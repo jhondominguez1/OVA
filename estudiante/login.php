7369a80baf81cc94ae57e4fffcaedd855229c704
Angie-Elizabeth <elizabethortiz1127@gmail.com>
2016-06-15 10:33:23 -0500

    modificación de login

--- estudiante/login.php
+++ estudiante/login.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if(!isset($_SESSION['id_usuario'])){ 
+    
+}
+?>
 <!DOCTYPE html>
 <html >
   <head>
@@ -78,14 +84,16 @@ input:focus { box-shadow: inset 0 -5px 45px rgba(100,100,100,0.4), 0 1px 1px rgb
 
   <body>
 
-    <div class="login">
+        <div class="login">
 	<h1>BIENVENID@S</h1>
     <form method="post" action="validarlogin.php">
-    	<input type="nombre_usuario" name="u" placeholder="Usuario" required="required" />
-        <input type="password" name="p" placeholder="Password" required="required" />
+    	<input type="text" name="nombre_usuario" id="nombre_usuario" placeholder="Usuario" required="required" />
+        <input type="password" name="password" id="password" placeholder="Password" required="required" />
         <button type="submit" class="btn btn-primary btn-block btn-large">ENVIAR</button>
     </form>
 </div>
+    
+        <s
     
         <script src="js/login.js"></script>
 
